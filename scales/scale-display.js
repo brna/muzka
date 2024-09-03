a8d9e4490c618e1953ba0d41cca593cca46e316c
brna <namestnik@gmail.com>
2024-09-04 01:22:27 +0200

    Update scale-display.js
    more scales

--- scales/scale-display.js
+++ scales/scale-display.js
@@ -1,4 +1,5 @@
 import { html, render } from "../libs/lit-3/lit-all.min.js";
+import { getScaleLetters } from "./scales-api.js";
 
 class ScaleDisplay extends HTMLElement {
   async connectedCallback() {
@@ -7,7 +8,18 @@ class ScaleDisplay extends HTMLElement {
   }
 
   render() {
-    render(html`<div>ScaleDisplay TODO</div> `, this, { host: this });
+    render(
+      html`<div>
+          F# minor scale: ${getScaleLetters("F#", "minor").join(", ")}
+        </div>
+        <div>
+          Bb harmonic minor scale:
+          ${getScaleLetters("Bb", "harmonic minor").join(", ")}
+        </div>
+        <div>F blues: ${getScaleLetters("F", "blues").join(", ")}</div> `,
+      this,
+      { host: this }
+    );
   }
 }
 
