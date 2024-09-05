1891b847eadba33b45dd6b42249dfe14d959cee6
brna <namestnik@gmail.com>
2024-09-05 16:50:55 +0200

    Update scale-display.js

--- scales/scale-display.js
+++ scales/scale-display.js
@@ -1,21 +1,44 @@
 import { html, render } from "../libs/lit-3/lit-all.min.js";
-import { getScaleLetters } from "./scales-api.js";
+import { getChromaticLetters, getScaleLetters } from "./scales-api.js";
 
 class ScaleDisplay extends HTMLElement {
   async connectedCallback() {
+    this.keys = getChromaticLetters();
+
+    this.key = "C";
+    this.scale = "major";
+
+    this.render();
+  }
+
+  onKeyClick(event) {
+    this.key = event.currentTarget.value;
     this.render();
   }
 
   render() {
     render(
-      html`<div>
-          F# minor scale: ${getScaleLetters("F#", "minor").join(", ")}
+      html`
+        <div class="row">
+          ${this.keys.map(
+            (key) =>
+              html`<div class="col"><button @click=${
+                this.onKeyClick
+              } value="${key}" type="button" class="btn btn-lg ${
+                key === this.key ? "btn-dark" : "btn-outline-dark"
+              }">${key}</div>`
+          )}
+          <div>
+            ${this.key} minor scale:
+            ${getScaleLetters(this.key, "minor").join(", ")}
+          </div>
+          <div>
+            Bb harmonic minor scale:
+            ${getScaleLetters("Bb", "harmonic minor").join(", ")}
+          </div>
+          <div>F blues: ${getScaleLetters("F", "blues").join(", ")}</div>
         </div>
-        <div>
-          Bb harmonic minor scale:
-          ${getScaleLetters("Bb", "harmonic minor").join(", ")}
-        </div>
-        <div>F blues: ${getScaleLetters("F", "blues").join(", ")}</div> `,
+      `,
       this,
       { host: this }
     );
