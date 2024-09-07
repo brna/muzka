31d076b244ecae3db63ae1794dd8a00988ed68b5
brna <namestnik@gmail.com>
2024-09-08 01:44:44 +0200

    Update scale-display.js
    harmony search param

--- scales/scale-display.js
+++ scales/scale-display.js
@@ -42,6 +42,8 @@ class ScaleDisplay extends HTMLElement {
     }
     this.key ??= "C";
 
+    this.harmony = searchParams.get("harmony");
+
     let scale = searchParams.get("scale");
     if (scale && this.scaleTypeNames.includes(scale)) {
       this.scaleType = getScaleTypeByName(scale);
@@ -53,12 +55,19 @@ class ScaleDisplay extends HTMLElement {
     this.updateUrl();
 
     this.render();
+
+    this.initHarmony();
   }
 
   getUrl(key, scale) {
     let searchParams = new URLSearchParams(window.location.search);
     searchParams.set("key", key);
     searchParams.set("scale", scale);
+    if (this.harmony) {
+      searchParams.set("harmony", this.harmony);
+    } else {
+      searchParams.delete("harmony");
+    }
     let loc = window.location;
     return `${loc.origin}${loc.pathname}?${searchParams.toString()}`;
   }
@@ -66,11 +75,18 @@ class ScaleDisplay extends HTMLElement {
   updateUrl() {
     let url = this.getUrl(this.key, this.scaleType.name);
     if (url !== window.location.href) {
-      window.location.href = url; // reload
+      // window.location.href = url; // reload
+      window.history.pushState(null, "", url);
     }
   }
 
   onScaleClick(event) {
+    console.log(
+      `scale-display.js: onScaleClick: dataset=`,
+      event.currentTarget.dataset
+    );
+    event.preventDefault();
+
     let dataset = event.currentTarget.dataset;
     if (!dataset.key && !dataset.scale) {
       return;
@@ -88,6 +104,55 @@ class ScaleDisplay extends HTMLElement {
     this.render();
   }
 
+  get allKeysHtml() {
+    return html`<div class="ps-2 pe-2">
+      <div
+        class="row p-2 bg-secondary"
+        style="padding-top:1px;padding-bottom:1px"
+      >
+        ${this.keys.map(
+          (key) =>
+            html`<a
+              title="Change key to ${key} ..."
+              @click=${this.onScaleClick}
+              data-key="${key}"
+              data-scale="${this.scaleType.name}"
+              href="${this.getUrl(key, this.scaleType.name)}"
+              class="col btn btn-sm w-100 ${key === this.key
+                ? "btn-primary"
+                : isNatural(key)
+                ? "btn-light"
+                : "btn-dark"}"
+              style="margin-left:0.5px;margin-right:0.5px"
+            >
+              ${key}
+            </a>`
+        )}
+      </div>
+    </div> `;
+  }
+
+  get allScalesHtml() {
+    return html`<div class="row p-2 pt-1 pb-1 bg-warning">
+      ${this.scaleTypeNames.map(
+        (name) =>
+          html`<a
+            title="Change scale to ${name} ..."
+            @click=${this.onScaleClick}
+            data-key="${this.key}"
+            data-scale="${name}"
+            href="${this.getUrl(this.key, name)}"
+            class="col btn btn-sm ms-1 ${name === this.scaleType.name
+              ? "btn-danger"
+              : "bg-warning-subtle"}"
+            style="margin-top:1px; margin-bottom:1px"
+          >
+            <span class="fs-6">${name}</span>
+          </a>`
+      )}
+    </div>`;
+  }
+
   get scaleHtml() {
     let tones = getScaleTypeTones(this.scaleType.name);
     let letters = getScaleLetters(this.key, this.scaleType.name, 1);
@@ -100,21 +165,30 @@ class ScaleDisplay extends HTMLElement {
 
     return html`
       ${tonalityLetter
-        ? html`<div class="" style="font-size:8pt">
+        ? html`<div class="" style="font-size:10pt">
             Tonality: ${tonalityLetter} major key
           </div>`
         : undefined}
       <table class="table mt-0 border border-5 fs-3">
-        <tr class="fs-6">
+        <tr class="fs-6 text-center">
           ${tones.map(
             (tone) => html`<th class="bg-secondary-subtle">${tone}</th>`
           )}
         </tr>
         <tr>
-          ${letters.map(
-            (letter) =>
-              html`<td class="fw-bold" data-key="${letter}">${letter}</td>`
-          )}
+          ${letters.map((letter) => {
+            return html`<td class="text-center">
+              <a
+                title="Change scale to ${letter} ${this.scaleType.name} ..."
+                @click=${this.onScaleClick}
+                data-key="${letter}"
+                data-scale="${this.scaleType.name}"
+                href="${this.getUrl(letter, this.scaleType.name)}"
+                class="btn btn-lg fw-bold w-100 border"
+                >${letter}</a
+              >
+            </td>`;
+          })}
         </tr>
       </table>
     `;
@@ -136,9 +210,13 @@ class ScaleDisplay extends HTMLElement {
           if (!modalScale) {
             return undefined;
           }
-          let url = this.getUrl(letter, modalScale.name);
-
-          return html` <a href="${url}" class="btn btn-sm btn-light"
+          return html` <a
+            title="Change scale to ${letter} ${modalScale.name} ..."
+            @click=${this.onScaleClick}
+            data-key="${letter}"
+            data-scale="${modalScale.name}"
+            href="this.getUrl(letter, modalScale.name)"
+            class="btn btn-sm btn-light border"
             >${convertToRoman(index + 1)}.<br />
             <span class="btn btn-light-outline"
               >${letter} ${modalScale.name}</span
@@ -149,55 +227,41 @@ class ScaleDisplay extends HTMLElement {
     </div>`;
   }
 
-  get thirdsHtml() {
-    return html` <table class="table mt-2 border border-5 fs-3">
-      <tr>
-        ${getScaleLetterPairs(this.key, this.scaleType.name, 2).map(
-          (letter) => html`<td class="bg-warning-subtle">${letter}</td>`
-        )}
-      </tr>
-      <tr>
-        ${getScaleLetters(this.key, this.scaleType.name, 1).map(
-          (letter) => html`<td class="bg-warning-subtle fw-bold">${letter}</td>`
-        )}
-      </tr>
-    </table>`;
-  }
-
-  get sixthsHtml() {
-    return html` <table class="table mt-2 border border-5 fs-3">
-      <tr>
-        ${getScaleLetters(this.key, this.scaleType.name, 1).map(
-          (letter) => html`<td class="bg-success-subtle fw-bold">${letter}</td>`
-        )}
-      </tr>
-      <tr>
-        ${getScaleLetterPairs(this.key, this.scaleType.name, -5).map(
-          (letter) => html`<td class="bg-success-subtle">${letter}</td>`
-        )}
-      </tr>
-    </table>`;
-  }
-
   getChordsHtml(shifts) {
     let chords = getScaleChords(this.key, this.scaleType.name, shifts);
+    let baseIndex = shifts.toReversed().findIndex((shift) => shift === 0);
     return html`<table class="table mt-2 border border-5 fs-3">
       <tr>
         ${chords.map(
-          (chord) => html`<td>${join(chord.toReversed(), html`<br />`)}</td>`
+          (chord) =>
+            html`<td>
+              ${chord
+                .toReversed()
+                .map(
+                  (letter, index) =>
+                    html`<div
+                      class="${index === baseIndex ? "fw-bold" : undefined}"
+                    >
+                      ${letter}
+                    </div>`
+                )}
+            </td>`
         )}
       </tr>
     </table>`;
   }
 
-  getAccordionItem(id, title, body, expanded) {
-    if (this.scaleType.tones.length < 7) {
-      return undefined;
-    }
-    return html`<div class="accordion-item">
+  getHarmonyAccordionItem(id, title, body) {
+    let expanded = this.harmony === id;
+    return html`<div
+      class="accordion-item"
+      id="harmony-accordion-item-${id}"
+      data-id="${id}"
+    >
       <h2 class="accordion-header">
         <button
           class="accordion-button ${!expanded ? "collapsed" : undefined}"
+          id="harmony-accordion-button-${id}"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#${id}"
@@ -209,115 +273,121 @@ class ScaleDisplay extends HTMLElement {
       </h2>
       <div
         id="${id}"
+        @click=${(event) => {
+          console.log("clicked " + id);
+          this.harmony = event.currentTarget.id;
+          this.updateUrl();
+        }}
         class="accordion-collapse collapse ${expanded ? "show" : undefined}"
-        data-bs-parent="#accordionTables"
+        data-bs-parent="#harmonyAccordion"
       >
         <div class="accordion-body">${body}</div>
       </div>
     </div>`;
   }
 
+  showHarmonyAccordionItem(id) {
+    let button = this.querySelector(`#harmony-accordion-button-${id}`);
+    if (button) {
+      button.click();
+    }
+  }
+
+  getHarmonyIds() {
+    let container = this.querySelector("#harmonyAccordion");
+    if (!container) {
+      return undefined;
+    }
+    return [...container.querySelectorAll(".accordion-item")].map(
+      (el) => el.dataset.id
+    );
+  }
+
+  initHarmony() {
+    let harmonyIds = this.getHarmonyIds();
+    this.harmony ??= harmonyIds[0];
+    if (!harmonyIds.includes(this.harmony)) {
+      this.harmony = harmonyIds[0];
+    }
+    this.updateUrl();
+    this.render();
+
+    for (let id of harmonyIds) {
+      let el = this.querySelector(`#${id}`);
+      el.addEventListener("shown.bs.collapse", () => {
+        this.harmony = id;
+        this.updateUrl();
+      });
+      el.addEventListener("hidden.bs.collapse", () => {
+        this.harmony = undefined;
+        this.updateUrl();
+      });
+    }
+  }
+
+  get harmonyAccordionHtml() {
+    return html` <div class="accordion mt-2" id="harmonyAccordion">
+      ${this.getHarmonyAccordionItem(
+        "h02",
+        "Thirds",
+        this.getChordsHtml([0, 2])
+      )}
+      ${this.getHarmonyAccordionItem(
+        "h20",
+        "Sixths",
+        this.getChordsHtml([2, 0])
+      )}
+      ${this.getHarmonyAccordionItem(
+        "h024",
+        "Triads",
+        this.getChordsHtml([0, 2, 4])
+      )}
+      ${this.getHarmonyAccordionItem(
+        "h240",
+        " Triads starting from the third",
+        this.getChordsHtml([2, 4, 0])
+      )}
+      ${this.getHarmonyAccordionItem(
+        "h402",
+        "Triads starting from the fifth",
+        this.getChordsHtml([4, 0, 2])
+      )}
+      ${this.getHarmonyAccordionItem(
+        "h0246",
+        "Tetrads",
+        this.getChordsHtml([0, 2, 4, 6])
+      )}
+      ${this.getHarmonyAccordionItem(
+        "h01245",
+        "Pentatonics 6/9",
+        this.getChordsHtml([0, 1, 2, 4, 5])
+      )}
+      ${this.getHarmonyAccordionItem(
+        "h01246",
+        "Pentatonics 7/9",
+        this.getChordsHtml([0, 1, 2, 4, 6])
+      )}
+    </div>`;
+  }
+
   render() {
     render(
       html`
-        <div class="ps-2 pe-2">
-          <div
-            class="row p-2 bg-secondary"
-            style="padding-top:1px;padding-bottom:1px"
-          >
-            ${this.keys.map(
-              (key) =>
-                html`<button
-                  @click=${this.onScaleClick}
-                  data-key="${key}"
-                  type="button"
-                  class="col btn btn-sm w-100 ${key === this.key
-                    ? "btn-primary"
-                    : isNatural(key)
-                    ? "btn-light"
-                    : "btn-dark"}"
-                  style="margin-left:0.5px;margin-right:0.5px"
-                >
-                  ${key}
-                </button>`
-            )}
-          </div>
-          <div class="row p-2 pt-1 pb-1 bg-warning">
-            ${this.scaleTypeNames.map(
-              (name) =>
-                html`<button
-                  @click=${this.onScaleClick}
-                  data-scale="${name}"
-                  type="button"
-                  class="col btn btn-sm ms-1 ${name === this.scaleType.name
-                    ? "btn-danger"
-                    : "bg-warning-subtle"}"
-                  style="margin-top:1px; margin-bottom:1px"
-                >
-                  <span class="fs-6">${name}</span>
-                </button>`
-            )}
-          </div>
+        ${this.allKeysHtml} ${this.allScalesHtml}
+        
           <h1 class="mt-2 text-center fw-bold">
             ${this.key} ${this.scaleType.name}
           </h1>
-          ${this.scaleType.aliases?.length
-            ? html`<div class="text-center fs-4">
-                (${this.scaleType.aliases.join(", ")})
-              </div>`
-            : undefined}
+          ${
+            this.scaleType.aliases?.length
+              ? html`<div class="text-center fs-4">
+                  (${this.scaleType.aliases.join(", ")})
+                </div>`
+              : undefined
+          }
           ${this.scaleHtml} ${this.modesHtml}
-
-          <div class="accordion" id="accordionTables">
-            ${this.getAccordionItem(
-              "thirdsTable",
-              "Thirds",
-              this.thirdsHtml,
-              true
-            )}
-            ${this.getAccordionItem(
-              "sixthsTable",
-              "Sixths",
-              this.sixthsHtml,
-              false
-            )}
-            ${this.getAccordionItem(
-              "triadsFromBaseTable",
-              "Triads",
-              this.getChordsHtml([0, 2, 4]),
-              false
-            )}
-            ${this.getAccordionItem(
-              "triadsFromThirdsTable",
-              " Triads starting from the third",
-              this.getChordsHtml([2, 4, 0]),
-              false
-            )}
-            ${this.getAccordionItem(
-              "triadsFromFifthsTable",
-              "Triads starting from the fifth",
-              this.getChordsHtml([4, 0, 2]),
-              false
-            )}
-            ${this.getAccordionItem(
-              "tetradsFromBaseTable",
-              "Tetrads",
-              this.getChordsHtml([0, 2, 4, 6]),
-              false
-            )}
-            ${this.getAccordionItem(
-              "pentatonics69",
-              "Pentatonics 6/9",
-              this.getChordsHtml([0, 1, 2, 4, 5]),
-              false
-            )}
-            ${this.getAccordionItem(
-              "pentatonics79",
-              "Pentatonics 7/9",
-              this.getChordsHtml([0, 1, 2, 4, 6]),
-              false
-            )}
-          </div>
+${this.harmonyAccordionHtml}
+         
         </div>
       `,
       this,
